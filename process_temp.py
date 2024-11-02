365f6e1ff1b3c48b7252f90020baf5f479e517a7
Leon <leonsuarez24@gmail.com>
2024-11-02 15:19:42 -0500

    txt logging

--- process_temp.py
+++ process_temp.py
@@ -1,63 +1,48 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import logging
 import os
 
 columns = ["CodigoEstacion", "Latitud", "Longitud", "Altitud", "Fecha", "Valor"]
 
+def write_to_file(file_path: str, message: str):
+    with open(file_path, "a") as file:
+        file.write(message + "\n")
 
 def processing_temp_data(data_path: str, columns: list) -> pd.DataFrame:
-
-    file_name = data_path.split("/")[-1]
-    file_name = file_name.split(".")[0]
-
-    if not os.path.exists("logs"):
-        os.makedirs("logs")
-
-    logging.basicConfig(
-        filename=f"logs/{file_name}.log",
-        level=logging.INFO,
-        format="%(asctime)s - %(message)s",
-    )
-
-    logging.info(f"loading data from {data_path}")
-
+    file_name = os.path.splitext(os.path.basename(data_path))[0]
+    log_path = f"logs/{file_name}.txt"
+    os.makedirs("logs", exist_ok=True)
+    
+    write_to_file(log_path, f"Loading data from {data_path}")
+    
     data = pd.read_excel(data_path)
-    logging.info(f"orginal dataset size: {data.shape}")
-
+    write_to_file(log_path, f"Original dataset size: {data.shape}")
+    
     data = data[columns]
     max_value = data["Valor"].max()
     min_value = data["Valor"].min()
 
-    logging.info(f"max value: {max_value}")
-    logging.info(f"min value: {min_value}")
-    logging.info(f"Number of NaN values: {data['Valor'].isna().sum()}")
-    logging.info("Deleting NaN values ...")
+    write_to_file(log_path, f"Max value: {max_value}")
+    write_to_file(log_path, f"Min value: {min_value}")
+    write_to_file(log_path, f"Number of NaN values: {data['Valor'].isna().sum()}")
+    write_to_file(log_path, "Deleting NaN values ...")
     data = data.dropna(subset=["Valor"])
-    logging.info(f"new dataset size: {data.shape}")
+    write_to_file(log_path, f"New dataset size: {data.shape}")
 
-    logging.info("Descriptive statistics ...")
-    logging.info(data["Valor"].describe())
-
+    write_to_file(log_path, "Descriptive statistics:")
+    write_to_file(log_path, str(data["Valor"].describe()))
+    
     data["Fecha"] = pd.to_datetime(data["Fecha"], format="%m/%d/%Y %H:%M")
     return data
 
-
 def filter_temp_data(tmax: pd.DataFrame, tmin: pd.DataFrame) -> pd.DataFrame:
-
-    if not os.path.exists("logs"):
-        os.makedirs("logs")
-
-    logging.basicConfig(
-        filename="logs/temperature.log",
-        level=logging.INFO,
-        format="%(message)s",
-    )
+    log_path = "logs/temperature.txt"
+    os.makedirs("logs", exist_ok=True)
 
     tmin_min = tmin["Valor"].min()
     tmax = tmax.drop(tmax[tmax["Valor"] < tmin_min].index)
-    logging.info(f"new dataset size with filter: {tmax.shape}")
+    write_to_file(log_path, f"New dataset size with filter: {tmax.shape}")
 
     fig, ax = plt.subplots(1, 2, figsize=(20, 6))
     ax[0].hist(tmax["Valor"], color="blue")
@@ -72,10 +57,10 @@ def filter_temp_data(tmax: pd.DataFrame, tmin: pd.DataFrame) -> pd.DataFrame:
     fig, ax = plt.subplots(1, 2, figsize=(20, 6))
     ax[0].set_title("Boxplot temperatura máxima")
     ax[0].set_ylabel("Temperatura °C")
-    bplot = ax[0].boxplot(tmax["Valor"], patch_artist=True)  # fill with color
+    ax[0].boxplot(tmax["Valor"], patch_artist=True)
     ax[1].set_title("Boxplot temperatura mínima")
     ax[1].set_ylabel("Temperatura °C")
-    bplot = ax[1].boxplot(tmin["Valor"], patch_artist=True)  # fill with color
+    ax[1].boxplot(tmin["Valor"], patch_artist=True)
     plt.savefig(f"logs/boxplot.png")
     plt.show()
 
