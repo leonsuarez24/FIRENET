2849df7d8acbc05cfb6519ee3965da85c3c9f038
leonsuarez24 <leonsuarez24@gmail.com>
2024-11-07 22:33:51 -0500

    update boundary and map

--- scripts/utils.py
+++ scripts/utils.py
@@ -5,18 +5,28 @@ import folium
 import streamlit as st
 from datetime import datetime
 import pandas as pd
+import geopandas as gpd
+import numpy as np
 
 
-def krigin_interpolation(data: pd.DataFrame):
-    df = pd.read_excel("data/tmean.xlsx")
+def get_santander_boundaries():
+    shapefile = "data/aoi/Departamento.shp"
+    df = gpd.read_file(shapefile)
+    santander = df[df["DeNombre"] == "Santander"]
+    santander = santander.to_crs("EPSG:4326")
+    boundary = santander.boundary
+    boundary_geo_json = boundary.to_json()
+
+    with open("data/santander_boundary.json", "w") as f:
+        f.write(boundary_geo_json)
 
 
 def display_map():
 
     df = pd.read_excel("data/tmean.xlsx")
 
-    min_date = df['Fecha'].min()
-    max_date = df['Fecha'].max()
+    min_date = df["Fecha"].min()
+    max_date = df["Fecha"].max()
 
     start_time = st.slider(
         "Seleccionar fecha",
@@ -28,33 +38,27 @@ def display_map():
 
     st.write(f"Fecha seleccionada: {start_time.strftime('%Y-%m')}")
 
-    selected_period = start_time.strftime('%Y-%m')
-    df['YearMonth'] = df['Fecha'].dt.to_period('M').astype(str)
-    df = df[df['YearMonth'] == selected_period]
+    selected_period = start_time.strftime("%Y-%m")
+    df["YearMonth"] = df["Fecha"].dt.to_period("M").astype(str)
+    df = df[df["YearMonth"] == selected_period]
 
     map = folium.Map(location=[7, -73.6536], zoom_start=8, tiles="CartoDB positron")
     folium.GeoJson(
-        "data/santander.geojson",
+        "data/santander_boundary.json",
         style_function=lambda feature: {
             "weight": 2,
         },
     ).add_to(map)
 
-    
     for _, row in df.iterrows():
         folium.CircleMarker(
-            location=(row['Latitud'], row['Longitud']),
+            location=(row["Latitud"], row["Longitud"]),
             radius=5,
-            color='blue',
+            color="blue",
             fill=True,
-            fill_color='blue',
+            fill_color="blue",
             fill_opacity=0.6,
-            popup=f"Date: {row['Fecha'].strftime('%Y-%m-%d')}, Valor_medio: {row['Valor_medio']}"
+            popup=f"Fecha: {row['Fecha'].strftime('%Y-%m-%d')}\nTemperatura media: {np.round(row['Valor_medio'], 2)} °C",
         ).add_to(map)
-
-
-
-
-
 
     st_map = st_folium(map, width=700, height=400)
